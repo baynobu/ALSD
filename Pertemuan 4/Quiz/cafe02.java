fb49c324548736463ece866a31e1e3896d5ed8c6
baynobu <darmawanhanif30@gmail.com>
2025-03-04 09:58:43 +0700

    upd: arr

--- Pertemuan 4/Quiz/cafe02.java
+++ Pertemuan 4/Quiz/cafe02.java
@@ -1,13 +1,19 @@
 public class cafe02 {
     public static void main(String[] args) {
-        cafeMain02 cafe = new cafeMain02("Cafe Santai", "Kopi Susu", 4.5, "Jakarta");
-        cafe.tampilkanInfo();
-        cafe.updateRating(4.8);
-        cafe.tampilkanInfo();
+        cafeMain02[] daftarCafe = new cafeMain02[3];
 
-        cafe = new cafeMain02("Cafe gak Santai", "Susu Kopi", 4.9, "Bandung");
-        cafe.tampilkanInfo();
+        daftarCafe[0] = new cafeMain02("Cafe Santai", "Kopi Susu", 4.5, "Jakarta");
+        daftarCafe[1] = new cafeMain02("Cafe Ceria", "Latte", 4.7, "Bandung");
+        daftarCafe[2] = new cafeMain02("Cafe Maju", "Espresso", 4.3, "Surabaya");
 
+        System.out.println("Informasi Semua Cafe:");
+        for (int i = 0; i < daftarCafe.length; i++) {
+            daftarCafe[i].tampilkanInfo();
+            System.out.println();
+        }
 
+        System.out.println("Memperbarui Rating Cafe Ceria:");
+        daftarCafe[1].updateRating(4.9);
+        daftarCafe[1].tampilkanInfo();
     }
 }
